8a246f433ff0118826be8dbf706fdd1fbe97de42
0oov <76262899+0oov@users.noreply.github.com>
2020-12-22 00:52:12 +0300

    Add files via upload

---
+++ ipkcho.py
@@ -0,0 +1,15 @@
+import socket
+import sys
+import os
+os.system("clear")
+print("""
+
+
+
+
+
+
+""")
+hi = input("\033[0;32m[\033[0;37m+\033[0;32m]\033[0;37mTarget > ")
+ip = socket.gethostbyname (hi)
+print ('\033[0;32m[\033[0;37m+\033[0;32m]\033[0;37mip > ',ip )
